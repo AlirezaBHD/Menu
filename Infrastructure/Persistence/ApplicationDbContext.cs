b98ad70dbeffead0f7f8432e7104cb481c2b6afd
AlirezaBHD <alirezabhd2@gmail.com>
2025-06-26 19:25:41 +0330

    Add ApplicationDbContext

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) {}
+
+    public DbSet<Restaurant> Restaurants => Set<Restaurant>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Section> Sections => Set<Section>();
+    public DbSet<MenuItem> MenuItems => Set<MenuItem>();
+}
