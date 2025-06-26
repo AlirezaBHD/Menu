ffcf942dc07d767d17cc2eec02265e069be4919b
AlirezaBHD <alirezabhd2@gmail.com>
2025-06-26 19:54:02 +0330

    Add base Repository functions and interface

---
+++ Domain/RepositoryInterfaces/IRepository.cs
@@ -0,0 +1,47 @@
+﻿namespace Domain.RepositoryInterfaces;
+
+public enum TrackingBehavior
+{
+    Default,
+    AsNoTracking,
+    AsNoTrackingWithIdentityResolution
+}
+
+public interface IRepository<T> where T : class
+{
+    #region Add Async
+
+    Task AddAsync(T entity);
+
+    #endregion
+
+    #region Remove
+
+    void Remove(T entity);
+
+    #endregion
+
+    #region Update
+
+    void Update(T entity);
+
+    #endregion
+
+    #region Save Async
+
+    Task SaveAsync();
+
+    #endregion
+
+    #region Get By Id Async
+
+    Task<T> GetByIdAsync(Guid id);
+
+    #endregion
+
+    #region Get IQuaryable
+
+    Task<IQueryable<T>> GetQueryable();
+
+    #endregion
+}
---
+++ Infrastructure/Repository/Repository.cs
@@ -0,0 +1,68 @@
+﻿using Domain.RepositoryInterfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repository;
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        #region Injections
+
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _entities;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _entities = context.Set<T>();
+        }
+
+        #endregion
+
+        #region Add Async
+
+        public async Task AddAsync(T entity) => await _entities.AddAsync(entity);
+
+        #endregion
+
+        #region Remove
+
+        public void Remove(T entity) => _entities.Remove(entity);
+
+        #endregion
+
+        #region Update
+
+        public void Update(T entity) => _entities.Update(entity);
+
+        #endregion
+
+        #region Save Async
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        #endregion
+
+        #region Get By Id Async
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            var obj = await _entities.FindAsync(id);
+            return obj!;
+        }
+
+        #endregion
+
+        #region Get Queryable
+
+        public Task<IQueryable<T>> GetQueryable()
+        {
+            var query = _context.Set<T>().AsQueryable();
+            return Task.FromResult(query);
+        }
+
+        #endregion
+        
+    }
