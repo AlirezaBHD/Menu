e1e5ffc1749ba9403afbf8d275317f3f685d6929
AlirezaBHD <alirezabhd2@gmail.com>
2025-06-27 14:21:12 +0330

    Create a DependencyInjection class to register Repositories and Services

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,30 @@
+using Application.Services;
+using Application.Services.Interfaces;
+using Domain.RepositoryInterfaces;
+using Infrastructure.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IMenuItemService, MenuItemService>();
+        services.AddScoped<ISectionService, SectionService>();
+        services.AddScoped<ICategoryService, CategoryService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationRepositories(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        services.AddScoped<IMenuItemRepository, MenuItemRepository>();
+        services.AddScoped<ISectionRepository, SectionRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        
+        return services;
+
+    }
+}
