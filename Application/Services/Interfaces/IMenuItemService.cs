9cbc81427fd67a2474199ca367962e20f77bc9eb
AlirezaBHD <alirezabhd2@gmail.com>
2025-06-27 17:05:28 +0330

    Create GetMenuItemsAsync function to get all MenuItems

--- Application/Services/Interfaces/IMenuItemService.cs
+++ Application/Services/Interfaces/IMenuItemService.cs
@@ -1,8 +1,9 @@
+using Application.Dto.Category;
 using Domain.Entities;
 
 namespace Application.Services.Interfaces;
 
 public interface IMenuItemService: IService<MenuItem>
 {
-    
+    Task<IEnumerable<CategoryDto>> GetMenuItemsAsync();
 }
--- Application/Services/MenuItemService.cs
+++ Application/Services/MenuItemService.cs
@@ -17,8 +17,20 @@ public class MenuItemService : Service<MenuItem>, IMenuItemService
     public MenuItemService(IMapper mapper,IMenuItemRepository menuItemRepository, ICategoryRepository categoryRepository)
         : base(mapper, menuItemRepository)
     {
-        _menuItemRepository = menuItemRepository;
-        _mapper = mapper;
+        _categoryRepository = categoryRepository;
+    }
+
+    #endregion
+
+    #region Get Menu Items
+
+    public async Task<IEnumerable<CategoryDto>> GetMenuItemsAsync()
+    {
+        var queryable = _categoryRepository.GetQueryable();
+        var query = queryable.Include(c => c.Sections).ThenInclude(s => s.MenuItems)
+;
+        var result = await query.ProjectTo<CategoryDto>(Mapper.ConfigurationProvider).ToListAsync();
+        return result;
     }
 
     #endregion
