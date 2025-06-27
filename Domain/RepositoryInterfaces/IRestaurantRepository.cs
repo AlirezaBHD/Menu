4aba6055af1f6f7eb3169f2c72bc30f329f0e6cc
AlirezaBHD <alirezabhd2@gmail.com>
2025-06-27 14:16:09 +0330

    Create Restaurant Repository

---
+++ Domain/RepositoryInterfaces/IRestaurantRepository.cs
@@ -0,0 +1,9 @@
+using Domain.Entities;
+
+namespace Domain.RepositoryInterfaces;
+
+
+public interface IRestaurantRepository: IRepository<Restaurant>
+{
+    
+}
---
+++ Infrastructure/Repository/RestaurantRepository.cs
@@ -0,0 +1,15 @@
+using Domain.Entities;
+using Domain.RepositoryInterfaces;
+using Infrastructure.Persistence;
+
+namespace Infrastructure.Repository;
+
+public class RestaurantRepository:Repository<Restaurant>, IRestaurantRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public RestaurantRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
