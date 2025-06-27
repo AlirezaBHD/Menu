d4e15e508f0c27fcdc4b1046c45e42436629524b
AlirezaBHD <alirezabhd2@gmail.com>
2025-06-27 23:26:05 +0330

    Add CreateCategory service function

--- Application/Services/CategoryService.cs
+++ Application/Services/CategoryService.cs
@@ -1,3 +1,4 @@
+using Application.Dto.Category;
 using Application.Services.Interfaces;
 using AutoMapper;
 using Domain.Entities;
@@ -15,4 +16,14 @@ public class CategoryService : Service<Category>, ICategoryService
     }
 
     #endregion
+
+    public async Task<CategoryResponse> CreateCategory(Guid restaurantId, CreateCategoryRequest createCategoryRequest)
+    {
+        var entity = Mapper.Map<CreateCategoryRequest, Category>(createCategoryRequest);
+        entity.RestaurantId = restaurantId;
+        await Repository.AddAsync(entity);
+        await Repository.SaveAsync();
+        var response = Mapper.Map<Category, CategoryResponse>(entity);
+        return response;
+    }
 }
--- Application/Services/Interfaces/ICategoryService.cs
+++ Application/Services/Interfaces/ICategoryService.cs
@@ -1,8 +1,9 @@
+using Application.Dto.Category;
 using Domain.Entities;
 
 namespace Application.Services.Interfaces;
 
 public interface ICategoryService: IService<Category>
 {
-    
+    Task<CategoryResponse> CreateCategory(Guid restaurantId, CreateCategoryRequest createCategoryRequest);
 }
