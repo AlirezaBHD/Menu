064f7c5315a1a9d841dc13d1e2007707598a27a2
AlirezaBHD <alirezabhd2@gmail.com>
2025-06-28 16:27:48 +0330

    Create CreateCategoryRequestValidator

---
+++ Application/Validations/Category/CreateCategoryRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using Application.Dto.Category;
+using Application.Extensions;
+using FluentValidation;
+
+namespace Application.Validations.Category;
+
+public class CreateCategoryRequestValidator : AbstractValidator<CreateCategoryRequest>
+{
+    public CreateCategoryRequestValidator()
+    {
+        var entityType = typeof(Domain.Entities.Category);
+        
+        RuleFor(x => x.Title)!
+            .ApplyLengthValidation(x => x.Title!, entityType);
+    }
+}
