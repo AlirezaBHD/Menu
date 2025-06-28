be3a51c2ee738179c44bae1757b98193b49abd23
AlirezaBHD <alirezabhd2@gmail.com>
2025-06-28 16:16:38 +0330

    Create Controller for Category

---
+++ API/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Dto.Category;
+using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/Restaurant/{restaurantId}/[controller]")]
+public class CategoryController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+
+    public CategoryController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+}
