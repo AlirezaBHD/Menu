ac60a427e379e2cdfcc4b302f8300456c48a996f
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-02 22:44:47 +0330

    Add Display attributes to MenuItem entity

--- Domain/Entities/MenuItem.cs
+++ Domain/Entities/MenuItem.cs
@@ -7,11 +7,20 @@ namespace Domain.Entities;
 public class MenuItem: BaseEntity
 {
     [MaxLength(50)]
+    [Display(Name = "عنوان")]
     public required string Title { get; set; }
-    [MaxLength(300)]
+    
+    [MaxLength(30)]
+    [Display(Name = "توضیحات")]
     public string? Description { get; set; }
+    
+    [Display(Name = "مبلغ")]
     public decimal Price { get; set; }
+    
+    [Display(Name = "مسیر عکس")]
     public string? ImagePath { get; set; }
+    
+    [Display(Name = "فعال بودن")]
     public bool IsAvailable { get; set; } = true;
     public Guid SectionId { get; set; }
     public Section? Section { get; set; }
