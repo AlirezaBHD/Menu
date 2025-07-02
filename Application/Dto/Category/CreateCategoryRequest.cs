8244fecabe786c623ab39ff33714bf4c26d2d13c
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-02 13:14:32 +0330

    bug fixe: change RestaurantId type from int to Guid

--- Application/Dto/Category/CreateCategoryRequest.cs
+++ Application/Dto/Category/CreateCategoryRequest.cs
@@ -2,6 +2,6 @@ namespace Application.Dto.Category;
 
 public class CreateCategoryRequest
 {
-    public int RestaurantId { get; set; }
+    public Guid RestaurantId { get; set; }
     public string? Title { get; set; }
 }
