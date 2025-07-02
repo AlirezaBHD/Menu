e2009bee8909f3d202f91d94b3e7afebd22371bd
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-02 12:19:53 +0330

    Add Sections property to CategoryResponse

--- Application/Dto/Category/CategoryResponse.cs
+++ Application/Dto/Category/CategoryResponse.cs
@@ -1,7 +1,10 @@
+using Application.Dto.Section;
+
 namespace Application.Dto.Category;
 
 public class CategoryResponse
 {
     public Guid Id { get; set; }
-    public string? Title { get; set; }
+    public string Title { get; set; }
+    public List<SectionDto> Sections { get; set; }
 }
