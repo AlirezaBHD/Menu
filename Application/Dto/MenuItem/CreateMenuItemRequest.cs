11a15e7fd7bb983f34c4e3363c971ded8dc15be4
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-02 15:50:24 +0330

    Add CreateMenuItemRequest

---
+++ Application/Dto/MenuItem/CreateMenuItemRequest.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Dto.MenuItem;
+
+public class CreateMenuItemRequest
+{
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public IFormFile ImageFile { get; set; }
+    public bool IsAvailable { get; set; }
+}
