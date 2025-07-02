fe1f478c4175823aba72ec4d6ccd12e0b7bb7284
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-02 22:49:12 +0330

    Add CreateMenuItemRequest validator

---
+++ Application/Validations/MenuItem/CreateMenuItemRequestValidator.cs
@@ -0,0 +1,26 @@
+using Application.Dto.MenuItem;
+using Application.Extensions;
+using FluentValidation;
+
+namespace Application.Validations.MenuItem;
+
+public class CreateMenuItemRequestValidator : AbstractValidator<CreateMenuItemRequest>
+{
+    public CreateMenuItemRequestValidator()
+    {
+        var entityType = typeof(Domain.Entities.MenuItem);
+
+        RuleFor(c => c.Title)!
+            .ApplyLengthValidation(dto => dto.Title!, entityType);
+
+        RuleFor(c => c.Description)!
+            .ApplyLengthValidation(dto => dto.Description!, entityType, blank: true);
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("قیمت باید بیشتر از صفر باشد.")
+            .LessThan(1_000_000_000).WithMessage("قیمت نمی‌تواند بیش از یک میلیارد ریال باشد.")
+            .PrecisionScale(18, 0, true).WithMessage("قیمت باید عددی صحیح باشد.");
+
+        RuleFor(c => c.ImageFile).ImageRule();
+    }
+}
