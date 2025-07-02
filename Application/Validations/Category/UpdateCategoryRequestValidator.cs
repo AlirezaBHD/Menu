f6a75cb08d3797ed24d1467cf6b901574ca94b94
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-02 12:37:18 +0330

    Add Validator for UpdateCategoryRequest

---
+++ Application/Validations/Category/UpdateCategoryRequestValidator.cs
@@ -0,0 +1,16 @@
+using Application.Dto.Category;
+using Application.Extensions;
+using FluentValidation;
+
+namespace Application.Validations.Category;
+
+public class UpdateCategoryRequestValidator : AbstractValidator<UpdateCategoryRequest>
+{
+    public UpdateCategoryRequestValidator()
+    {
+        var entityType = typeof(Domain.Entities.Category);
+        
+        RuleFor(c => c.Title)!
+            .ApplyLengthValidation(dto => dto.Title!, entityType);
+    }
+}
