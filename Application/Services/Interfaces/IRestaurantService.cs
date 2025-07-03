762f236b39689c68ba3141bccd7de6bf585a6568
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-03 11:21:52 +0330

    Implement UpdateRestaurantAsync functionality

--- Application/Services/Interfaces/IRestaurantService.cs
+++ Application/Services/Interfaces/IRestaurantService.cs
@@ -6,4 +6,5 @@ namespace Application.Services.Interfaces;
 public interface IRestaurantService : IService<Restaurant>
 {
     Task CreateRestaurantAsync(CreateRestaurantRequest createRestaurantRequest);
+    Task UpdateRestaurantAsync(Guid id, UpdateRestaurantRequest dto);
 }
--- Application/Services/RestaurantService.cs
+++ Application/Services/RestaurantService.cs
@@ -10,10 +10,13 @@ namespace Application.Services;
 public class RestaurantService :Service<Restaurant>, IRestaurantService
 {
     #region Injection
+    
+    private readonly IFileService _fileService;
 
-    public RestaurantService(IRestaurantRepository restaurantRepository, IMapper mapper) 
+    public RestaurantService(IRestaurantRepository restaurantRepository, IMapper mapper, IFileService fileService) 
         : base(mapper, restaurantRepository)
     {
+        _fileService = fileService;
     }
 
     #endregion
@@ -23,5 +26,21 @@ public class RestaurantService :Service<Restaurant>, IRestaurantService
         var entity = Mapper.Map<CreateRestaurantRequest, Restaurant>(createRestaurantRequest);
         await Repository.AddAsync(entity);
         await Repository.SaveAsync();
+    }
+    
+    
+    public async Task UpdateRestaurantAsync(Guid id, UpdateRestaurantRequest dto)
+    {
+        var restaurant = await Repository.GetByIdAsync(id);
+        restaurant = Mapper.Map(dto, restaurant);
+
+        if (dto.LogoFile != null)
+        {
+            var imagePath = await _fileService.SaveFileAsync(dto.LogoFile, "Restaurant");
+            restaurant.LogoPath = imagePath;
+        }
+
+        Repository.Update(restaurant);
+        await Repository.SaveAsync();    
     }
 }
