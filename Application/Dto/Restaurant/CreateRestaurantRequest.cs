e3d8818bededf6da5ebe585195499df5cab354e7
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-03 11:06:27 +0330

    Add CreateRestaurantRequest

---
+++ Application/Dto/Restaurant/CreateRestaurantRequest.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Dto.Restaurant;
+
+public class CreateRestaurantRequest
+{
+    public string Name { get; set; }
+    public Guid OwnerId { get; set; }
+    public string Address { get; set; }
+    public string? Description { get; set; }
+    public IFormFile LogoFile { get; set; }
+    public Dictionary<string, string> OpeningHours { get; set; }
+}
