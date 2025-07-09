f294cf6e1d5e6164c52b5ce1b4723b3ba794a684
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-09 18:21:22 +0330

    Add Management Controller

---
+++ API/Controllers/ManagementController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ManagementController : ControllerBase
+{
+    private readonly IManagementService _managementService;
+
+    public ManagementController(IManagementService managementService)
+    {
+        _managementService = managementService;
+    }
+}
