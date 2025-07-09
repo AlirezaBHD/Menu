7814c4d6db5a113b39c8e2067d7e38b9ff91f635
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-09 12:50:02 +0330

    Map RegisterAdminRequest to ApplicationUser

---
+++ Application/Mappings/AuthProfile.cs
@@ -0,0 +1,15 @@
+﻿using Application.Dto.Authentication;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappings;
+
+public class AuthProfile : Profile
+{
+    public AuthProfile()
+    {
+        CreateMap<RegisterAdminRequest, ApplicationUser>()
+            .ForMember(dest => dest.UserName, opt =>
+            opt.MapFrom(src => src.Username));
+    }
+}
