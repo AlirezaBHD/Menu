254b0a567b4ce29d5338fe38ea5e5d46a5d71215
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-10 15:09:44 +0330

    Write logs for Auth service

--- Application/Services/AuthService.cs
+++ Application/Services/AuthService.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Services;
 
@@ -18,17 +19,20 @@ public class AuthService: IAuthService
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly IConfiguration _config;
     private readonly IMapper _mapper;
+    private readonly ILogger<AuthService> _logger;
     
-    public AuthService(UserManager<ApplicationUser> userManager, IMapper mapper, IConfiguration config, SignInManager<ApplicationUser> signInManager)
+    public AuthService(UserManager<ApplicationUser> userManager, IMapper mapper, IConfiguration config, SignInManager<ApplicationUser> signInManager, ILogger<AuthService> logger)
     {
         _userManager = userManager;
         _mapper = mapper;
         _config = config;
         _signInManager = signInManager;
+        _logger = logger;
     }
 
     public async Task CreateAdminAsync(RegisterAdminRequest request)
     {
+        _logger.LogInformation("Creating admin attempt for username: {Username}", request.Username);
         var existingUser = await _userManager.FindByNameAsync(request.Username);
         if (existingUser != null)
         {
@@ -40,18 +44,26 @@ public class AuthService: IAuthService
         {
             throw new ValidationException("در فرایند ثبت ادمین خطایی رخ داد");
         }
+        _logger.LogInformation("Admin created successfully. username: {Username}", request.Username);
     }
 
     public async Task<LoginResponse> LoginAsync(LoginRequest request)
     {
+        _logger.LogInformation("Login attempt for username: {Username}", request.Username);
         var user = await _userManager.FindByNameAsync(request.Username);
         if (user == null)
+        {
+            _logger.LogWarning("Login failed. Username not found: {Username}", request.Username);
             throw new ValidationException("نام کاربری یا رمز عبور اشتباه است");
+        }
 
         var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
         if (!result.Succeeded)
+        {
+            _logger.LogWarning("Login failed. Wrong password for username: {Username}", request.Username);
             throw new ValidationException("نام کاربری یا رمز عبور اشتباه است");
-        
+        }
+
         var token = GenerateJwtToken(user);
         return token;
     }
