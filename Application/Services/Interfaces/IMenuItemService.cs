5816d76680f19b7de8647ac31f205dfe943f8caf
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-14 17:31:37 +0330

    Remove GetRestaurantMenuAsync from MenuItemService

--- Application/Services/Interfaces/IMenuItemService.cs
+++ Application/Services/Interfaces/IMenuItemService.cs
@@ -1,12 +1,11 @@
-using Application.Dto.Category;
 using Application.Dto.MenuItem;
+using Application.Dto.Restaurant;
 using Domain.Entities;
 
 namespace Application.Services.Interfaces;
 
 public interface IMenuItemService: IService<MenuItem>
 {
-    Task<IEnumerable<RestaurantMenuDto>> GetRestaurantMenuAsync(Guid restaurantId);
     Task<MenuItemResponse> CreateMenuItemAsync(Guid sectionId, CreateMenuItemRequest createMenuItemRequest);
     Task DeleteMenuItemAsync(Guid id);
     Task UpdateMenuItemAsync(Guid id, UpdateMenuItemRequest dto);
--- Application/Services/MenuItemService.cs
+++ Application/Services/MenuItemService.cs
@@ -1,5 +1,5 @@
-using Application.Dto.Category;
 using Application.Dto.MenuItem;
+using Application.Dto.Restaurant;
 using Application.Services.Interfaces;
 using AutoMapper;
 using Domain.Entities;
@@ -12,31 +12,17 @@ public class MenuItemService : Service<MenuItem>, IMenuItemService
 {
     #region Injection
 
-    private readonly ICategoryService _categoryService;
     private readonly IFileService _fileService;
 
-    public MenuItemService(IMapper mapper, IMenuItemRepository menuItemRepository, ICategoryService categoryService,
+    public MenuItemService(IMapper mapper, IMenuItemRepository menuItemRepository,
         IFileService fileService, ILogger<MenuItem> logger)
         : base(mapper, menuItemRepository, logger)
     {
-        _categoryService = categoryService;
         _fileService = fileService;
     }
 
     #endregion
-
-    #region Get Restaurant Menu Async
-
-    public async Task<IEnumerable<RestaurantMenuDto>> GetRestaurantMenuAsync(Guid restaurantId)
-    {
-        var result = await _categoryService.GetAllProjectedAsync<RestaurantMenuDto>(
-            predicate: c => c.RestaurantId == restaurantId);
-        return result;
-    }
-
-    #endregion
-
-
+    
     public async Task<MenuItemResponse> CreateMenuItemAsync(Guid sectionId, CreateMenuItemRequest createMenuItemRequest)
     {
         var entity = Mapper.Map<CreateMenuItemRequest, MenuItem>(createMenuItemRequest);
