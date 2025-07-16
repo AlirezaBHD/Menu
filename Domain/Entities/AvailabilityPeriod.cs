7c3280a065c732b676f2f350cedf182fdb647ada
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-16 18:53:17 +0330

    Add AvailabilityPeriod entity with properties and AvailabilityEnum.

---
+++ Domain/Entities/AvailabilityPeriod.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class AvailabilityPeriod
+{
+    [Display(Name = "فعال بودن")]
+    public bool IsAvailable { get; set; } = true;
+    
+    [Display(Name = "وضعیت فعالیت")]
+    public AvailabilityEnum AvailabilityType { get; set; } = AvailabilityEnum.Available;
+    
+    [Display(Name = "ساعت شروع")]
+    public TimeOnly FromTime { get; set; } = TimeOnly.MinValue;
+    
+    [Display(Name = "ساعت پایان")]
+    public TimeOnly ToTome { get; set; } = TimeOnly.MaxValue;
+}
+
+public enum AvailabilityEnum
+{
+    Available = 1,
+    Unavailable = 2
+}
