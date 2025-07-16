c54b9ad7b920194b337c5ebf40755dc68030425d
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-16 18:54:47 +0330

    Adds ModelBuilderExtensions to configure AvailabilityPeriod properties.

---
+++ Infrastructure/Persistence/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void ConfigureAvailabilityPeriod<T>(this OwnedNavigationBuilder<T, AvailabilityPeriod> ap)
+        where T : class
+    {
+        ap.Property(p => p.IsAvailable).HasColumnName("is_available");
+        ap.Property(p => p.AvailabilityType).HasColumnName("availability_type");
+        ap.Property(p => p.FromTime).HasColumnName("from_time");
+        ap.Property(p => p.ToTome).HasColumnName("to_time");
+    }
+}
