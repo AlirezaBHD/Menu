48a530a037307bf6f736325b012eb80056330eef
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-20 16:50:35 +0330

    Add AvailabilityPeriod to CreateCategoryRequest DTO.

--- Application/Dto/Category/CreateCategoryRequest.cs
+++ Application/Dto/Category/CreateCategoryRequest.cs
@@ -1,6 +1,9 @@
+using Application.Dto.AvailabilityPeriod;
+
 namespace Application.Dto.Category;
 
 public class CreateCategoryRequest
 {
     public string Title { get; set; }
+    public AvailabilityPeriodRequest AvailabilityPeriod { get; set; }
 }
