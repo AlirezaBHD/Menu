fbac63c154534336104187b70d1bb5e3126e5c2f
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-20 16:52:11 +0330

    Add availability period to the category response DTO.

--- Application/Dto/Category/CategoryResponse.cs
+++ Application/Dto/Category/CategoryResponse.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Application.Dto.AvailabilityPeriod;
 using Application.Dto.Section;
 
 namespace Application.Dto.Category;
@@ -8,5 +9,6 @@ public class CategoryResponse
     public Guid Id { get; set; }
     [Required]
     public string Title { get; set; }
+    public AvailabilityPeriodResponse AvailabilityPeriod { get; set; }
     public List<SectionDto> Sections { get; set; } = [];
 }
