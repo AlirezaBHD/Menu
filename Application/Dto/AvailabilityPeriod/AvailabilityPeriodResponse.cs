a71e3a2d879c65eaaccb4d45c58ca609c615c8bb
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-20 16:48:27 +0330

    Add AvailabilityPeriodRequest DTO to encapsulate availability data.

---
+++ Application/Dto/AvailabilityPeriod/AvailabilityPeriodResponse.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Dto.AvailabilityPeriod;
+
+public class AvailabilityPeriodResponse
+{
+    public bool IsAvailable { get; set; }
+    public AvailabilityEnum AvailabilityType { get; set; }
+    public string AvailabilityTypeName { get; set; }
+    public TimeSpan FromTime { get; set; }
+    public TimeSpan ToTime { get; set; }
+}
