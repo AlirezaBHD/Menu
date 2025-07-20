69a28274e9344b0e39cc843307aa57b1912cb677
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-20 16:51:20 +0330

    Add availability period property to update category request.

--- Application/Dto/Category/UpdateCategoryRequest.cs
+++ Application/Dto/Category/UpdateCategoryRequest.cs
@@ -1,7 +1,10 @@
+using Application.Dto.AvailabilityPeriod;
+
 namespace Application.Dto.Category;
 
 public class UpdateCategoryRequest
 {
     public string Title { get; set; }
     public List<Guid> SectionIds { get; set; }
+    public AvailabilityPeriodRequest AvailabilityPeriod { get; set; }
 }
