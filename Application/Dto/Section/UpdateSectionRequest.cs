6c57a6fe22a57f45d068580466678f05b21cdbfe
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-20 16:53:10 +0330

    Add availability period to section update request.

--- Application/Dto/Section/UpdateSectionRequest.cs
+++ Application/Dto/Section/UpdateSectionRequest.cs
@@ -1,7 +1,10 @@
+using Application.Dto.AvailabilityPeriod;
+
 namespace Application.Dto.Section;
 
 public class UpdateSectionRequest
 {
     public string Title { get; set; }
     public List<Guid> MenuItemIds { get; set; }
+    public AvailabilityPeriodRequest AvailabilityPeriod { get; set; }
 }
