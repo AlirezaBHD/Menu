c0426728c52b36e5d4919445b35d17f42941515b
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-20 16:52:41 +0330

    Add AvailabilityPeriod to CreateSectionRequest for scheduling.

--- Application/Dto/Section/CreateSectionRequest.cs
+++ Application/Dto/Section/CreateSectionRequest.cs
@@ -1,6 +1,9 @@
+using Application.Dto.AvailabilityPeriod;
+
 namespace Application.Dto.Section;
 
 public class CreateSectionRequest
 {
     public string? Title { get; set; }
+    public AvailabilityPeriodRequest AvailabilityPeriod { get; set; }
 }
