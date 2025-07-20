3823bc2e8817ed6a3db0b83c60f025e4928366e2
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-20 19:03:32 +0330

    Add MenuItemVariant entity with properties and display attributes.

---
+++ Domain/Entities/MenuItemVariant.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+[DisplayName("نوع آیتم منو")]
+public class MenuItemVariant : BaseEntity
+{
+    public Guid MenuItemId { get; set; }
+    public MenuItem MenuItem { get; set; } = null!;
+
+    [MaxLength(100)]
+    [Display(Name = "عنوان نوع")]
+    public string Title { get; set; } = "";
+
+    [MaxLength(300)]
+    [Display(Name = "توضیحات")]
+    public string Description { get; set; }  = "";
+
+    [Display(Name = "قیمت")]
+    public decimal Price { get; set; }
+
+    [Display(Name = "موجودی")]
+    public bool IsAvailable { get; set; } = true;
+}
