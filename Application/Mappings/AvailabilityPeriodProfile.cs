874a6b1eea89b9fc6981b7b4e2d6bd381f2cfe45
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-20 16:50:08 +0330

    Add automapper profile for availability period entities and DTOs.

---
+++ Application/Mappings/AvailabilityPeriodProfile.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dto.AvailabilityPeriod;
+using Application.Extensions;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappings;
+
+public class AvailabilityPeriodProfile : Profile
+{
+    public AvailabilityPeriodProfile()
+    {
+        CreateMap<AvailabilityPeriodRequest, AvailabilityPeriod>();
+        CreateMap<AvailabilityPeriod, AvailabilityPeriodResponse>()
+            .ForMember(dest => dest.AvailabilityTypeName,
+                opt => opt.MapFrom(src => src.AvailabilityType.GetDisplayName()));
+    }
+}
