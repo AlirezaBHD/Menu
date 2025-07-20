1e5bd50db4b62305785fb7225eaee608ec62c48b
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-20 18:55:14 +0330

    Add IsAvailable property to MenuItemDto for menu item availability.

--- Application/Dto/MenuItem/MenuItemDto.cs
+++ Application/Dto/MenuItem/MenuItemDto.cs
@@ -10,4 +10,5 @@ public class MenuItemDto
     public string? Description { get; set; }
     public decimal Price { get; set; }
     public string? ImagePath { get; set; }
+    public bool IsAvailable { get; set; }
 }
