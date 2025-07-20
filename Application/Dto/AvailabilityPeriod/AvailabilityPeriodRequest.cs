e7deb2daaf92e61647966a26ef956626ecf6b35f
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-20 16:48:11 +0330

    Add AvailabilityPeriodRequest DTO to represent availability data.

---
+++ Application/Dto/AvailabilityPeriod/AvailabilityPeriodRequest.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Application.Dto.AvailabilityPeriod;
+
+public class AvailabilityPeriodRequest
+{
+    public bool IsAvailable { get; set; }
+    public AvailabilityEnum AvailabilityType { get; set; }
+    public TimeSpan FromTime { get; set; }
+    public TimeSpan ToTime { get; set; }
+}
