824283357f27baf410381921df9bab9f4a260285
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-21 11:45:47 +0330

    Add MenuItemVariantProfile for mapping MenuItemVariant and its DTO.

---
+++ Application/Mappings/MenuItemVariantProfile.cs
@@ -0,0 +1,14 @@
+﻿using Application.Dto.MenuItemVariant;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappings;
+
+public class MenuItemVariantProfile : Profile
+{
+    public MenuItemVariantProfile()
+    {
+        CreateMap<MenuItemVariantDto,MenuItemVariant>();
+        CreateMap<MenuItemVariant,MenuItemVariantDto>();
+    }
+}
