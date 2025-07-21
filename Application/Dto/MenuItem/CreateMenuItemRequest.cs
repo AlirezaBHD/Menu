105db5fa6acc166ab75007ff7a4ec7d5ce821d71
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-21 11:48:58 +0330

    Adds MenuItemVariantDto and JsonModelBinder for menu item creation.

--- Application/Dto/MenuItem/CreateMenuItemRequest.cs
+++ Application/Dto/MenuItem/CreateMenuItemRequest.cs
@@ -1,5 +1,9 @@
 using Application.Dto.ActivityPeriod;
+using Application.Dto.MenuItemVariant;
+using Application.Extensions;
+using Domain.Entities;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Dto.MenuItem;
 
@@ -7,8 +11,10 @@ public class CreateMenuItemRequest
 {
     public string Title { get; set; }
     public string? Description { get; set; }
-    public decimal Price { get; set; }
     public IFormFile ImageFile { get; set; }
     public bool IsAvailable { get; set; }
     public ActivityPeriodRequest ActivityPeriod { get; set; }
+    
+    [ModelBinder(BinderType = typeof(JsonModelBinder))]
+    public List<MenuItemVariantDto> Variants { get; set; }
 }
