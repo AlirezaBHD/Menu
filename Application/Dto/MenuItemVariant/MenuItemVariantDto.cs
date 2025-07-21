4adec4c7317175677b20e2fbc74dceda90ec7fc5
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-21 11:45:27 +0330

    Add MenuItemVariantDto to represent menu item variant data.

---
+++ Application/Dto/MenuItemVariant/MenuItemVariantDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Dto.MenuItemVariant;
+
+public class MenuItemVariantDto
+{
+    public string Detail { get; set; }  = "";
+
+    public decimal Price { get; set; }
+
+    public bool IsAvailable { get; set; }
+}
