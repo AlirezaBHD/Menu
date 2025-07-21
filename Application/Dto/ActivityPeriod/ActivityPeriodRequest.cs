091069121c97539e9783ffcd0656e786a079f63c
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-21 11:46:33 +0330

    Rename `IsAvailable` to `IsActive` in ActivityPeriodRequest.

--- Application/Dto/ActivityPeriod/ActivityPeriodRequest.cs
+++ Application/Dto/ActivityPeriod/ActivityPeriodRequest.cs
@@ -4,7 +4,7 @@ namespace Application.Dto.ActivityPeriod;
 
 public class ActivityPeriodRequest
 {
-    public bool IsAvailable { get; set; }
+    public bool IsActive { get; set; }
     public ActivityEnum ActivityEnum { get; set; }
     public TimeSpan FromTime { get; set; }
     public TimeSpan ToTime { get; set; }
