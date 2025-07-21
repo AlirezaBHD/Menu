a459e5201c881cc72f2f7195f0a3fe22f3dd9387
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-21 11:49:59 +0330

    Remove price validation rules from update menu item request.

--- Application/Validations/MenuItem/UpdateMenuItemRequestValidator.cs
+++ Application/Validations/MenuItem/UpdateMenuItemRequestValidator.cs
@@ -17,11 +17,6 @@ public class UpdateMenuItemRequestValidator : AbstractValidator<UpdateMenuItemRe
         RuleFor(c => c.Description)!
             .LengthValidationRule(dto => dto.Description!, entityType, blank: true);
 
-        RuleFor(x => x.Price)
-            .GreaterThan(0).WithMessage("قیمت باید بیشتر از صفر باشد.")
-            .LessThan(1_000_000_000).WithMessage("قیمت نمی‌تواند بیش از یک میلیارد ریال باشد.")
-            .PrecisionScale(18, 0, true).WithMessage("قیمت باید عددی صحیح باشد.");
-
         RuleFor(c => c.ImageFile)!.ImageFileRule(blank: true);
         
         RuleFor(x => x.ActivityPeriod)
