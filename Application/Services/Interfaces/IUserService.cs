4bcc71d75962a2f4bc28ee761e1653fa0c976d1e
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-22 10:48:00 +0330

    Implements UserService and IUserService with basic dependency injection.

---
+++ Application/Services/Interfaces/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Services.Interfaces;
+
+public interface IUserService: IService<ApplicationUser>
+{
+    
+}
---
+++ Application/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Services;
+
+public class UserService : Service<ApplicationUser>, IUserService
+{
+    #region Injection
+
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IMapper mapper, ILogger<ApplicationUser> logger, IUserRepository userRepository)
+        : base(mapper, userRepository, logger)
+    {
+        _userRepository = userRepository;
+    }
+
+    #endregion
+}
