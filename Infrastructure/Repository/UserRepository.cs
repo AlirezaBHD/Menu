fb52e1b32e0e0b0104777313c5baef67d4cddebf
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-22 10:47:31 +0330

    Implements UserRepository and IUserRepository.

---
+++ Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Domain.Interfaces.Services;
+using Infrastructure.Persistence;
+
+namespace Infrastructure.Repository;
+
+public class UserRepository:Repository<ApplicationUser>, IUserRepository
+{
+    #region Injection
+    
+    private readonly ApplicationDbContext _context;
+    private readonly ICurrentUser _currentUser;
+
+    public UserRepository(ApplicationDbContext context, ICurrentUser currentUser) : base(context)
+    {
+        _context = context;
+        _currentUser = currentUser;
+    }
+    
+    #endregion
+}
