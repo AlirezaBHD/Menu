f1a4a41e57085e08732108644e13639e1ca460c8
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-22 11:04:06 +0330

    Add user controller to manage user-related endpoints.

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+}
