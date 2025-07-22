75071dfba72a45e4642d0970e8ececfc839cf244
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-22 15:02:25 +0330

    Implement SectionListResponse DTO.

---
+++ Application/Dto/Section/SectionListResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Dto.Section;
+
+public class SectionListResponse
+{
+    public Guid Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    public int Order { get; set; }
+    public string CategoryName { get; set; }
+}
