43b42cee7082874ed8711c34cb24f184aa4354fd
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-22 11:06:00 +0330

    Implements endpoint to get user's restaurants.

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using Application.Services.Interfaces;
+﻿using Application.Dto.User;
+using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace API.Controllers;
 
@@ -13,4 +16,14 @@ public class UserController : ControllerBase
     {
         _userService = userService;
     }
+    
+    [Authorize]
+    [SwaggerResponse(200, "User's restaurants", typeof(IEnumerable<UserRestaurantsDto>))]
+    [HttpGet("restaurants")]
+    public async Task<IActionResult> GetUsersRestaurants()
+    {
+        var result = await _userService.Restaurants();
+        return Ok(result);
+    }
+    
 }
