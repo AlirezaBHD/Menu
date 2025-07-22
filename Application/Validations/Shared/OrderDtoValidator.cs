db67f48f13d90c14f932be9bd6ca78fb3dca461a
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-22 14:59:50 +0330

    Adds OrderDtoValidator to validate order number for uniqueness and positivity.

---
+++ Application/Validations/Shared/OrderDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿using Application.Dto.Shared;
+using FluentValidation;
+
+namespace Application.Validations.Shared;
+
+public class OrderDtoValidator : AbstractValidator<List<OrderDto>>
+{
+    public OrderDtoValidator()
+    {
+        var entityType = typeof(Domain.Entities.Section);
+        
+        RuleFor(x => x)
+            .Custom((list, context) =>
+            {
+                var invalidOrders = list.Where(d => d.Order < 1).ToList();
+                if (invalidOrders.Any())
+                {
+                    context.AddFailure("تمام جایگاه‌ها باید عددی مثبت و بزرگتر از صفر باشند.");
+                }
+
+                var duplicateOrders = list
+                    .GroupBy(d => d.Order)
+                    .Where(g => g.Count() > 1)
+                    .Select(g => g.Key)
+                    .ToList();
+
+                if (duplicateOrders.Any())
+                {
+                    context.AddFailure("عدد تکراری به عنوان جایگاه غیرقابل قبول است");
+                }
+            });
+    }
+}
