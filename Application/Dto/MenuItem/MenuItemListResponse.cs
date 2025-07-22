21946a623c3091c28a58e5c470d28c1d751b2e9b
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-22 15:32:51 +0330

    Add MenuItemListResponse DTO to represent menu item list data.

---
+++ Application/Dto/MenuItem/MenuItemListResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Dto.MenuItem;
+
+public class MenuItemListResponse
+{
+    public Guid Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    public int Order { get; set; }
+    public string SectionTitle { get; set; }
+}
