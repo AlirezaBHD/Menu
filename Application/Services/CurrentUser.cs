2e5d76360b3a927bb2fa4d123042daa886a3baa4
AlirezaBHD <alirezabhd2@gmail.com>
2025-07-22 12:33:42 +0330

    Adds RestaurantId to ICurrentUser and retrieves it from the session.

--- Application/Services/CurrentUser.cs
+++ Application/Services/CurrentUser.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using Application.Exceptions;
 using Domain.Interfaces.Services;
 using Microsoft.AspNetCore.Http;
 
@@ -17,4 +18,13 @@ public class CurrentUser : ICurrentUser
         Guid.TryParse(_httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier), out var id)
             ? id
             : null;
+
+    public Guid RestaurantId => GetCurrentRestaurantId();
+    private Guid GetCurrentRestaurantId()
+    {
+        var value = _httpContextAccessor.HttpContext?.Session.GetString("CurrentRestaurantId");
+        if (Guid.TryParse(value, out var restaurantId))
+            return restaurantId;
+        throw new ForbiddenException();
+    }
 }
--- Domain/Interfaces/Services/ICurrentUser.cs
+++ Domain/Interfaces/Services/ICurrentUser.cs
@@ -3,4 +3,5 @@ namespace Domain.Interfaces.Services;
 public interface ICurrentUser
 {
     Guid? UserId { get; }
+    Guid RestaurantId { get; }
 }
