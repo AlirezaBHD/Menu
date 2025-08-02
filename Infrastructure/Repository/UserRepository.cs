7738923665c9ec1eba3bc8a820b9314f9e259758
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-02 19:04:43 +0330

    Implements user lookup and creation methods in UserRepository.

--- Infrastructure/Repository/UserRepository.cs
+++ Infrastructure/Repository/UserRepository.cs
@@ -2,21 +2,50 @@
 using Domain.Interfaces.Repositories;
 using Domain.Interfaces.Services;
 using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Repository;
 
-public class UserRepository:Repository<User>, IUserRepository
+public class UserRepository : Repository<User>, IUserRepository
 {
     #region Injection
-    
+
     private readonly ApplicationDbContext _context;
     private readonly ICurrentUser _currentUser;
+    private readonly ILogger<UserRepository> _logger;
 
-    public UserRepository(ApplicationDbContext context, ICurrentUser currentUser) : base(context)
+    public UserRepository(ApplicationDbContext context, ICurrentUser currentUser, ILogger<UserRepository> logger) :
+        base(context)
     {
         _context = context;
         _currentUser = currentUser;
+        _logger = logger;
     }
-    
+
     #endregion
+
+    public async Task<User?> FindByUsernameAsync(string requestUsername)
+    {
+        var queryable = GetQueryable();
+
+        var user = await queryable.Include(u => u.Roles)
+            .FirstOrDefaultAsync(u => u.NormalizedUsername == requestUsername.ToUpper());
+        return user;
+    }
+
+    public async Task<bool> AddUserAsync(User user)
+    {
+        try
+        {
+            await AddAsync(user);
+            await SaveAsync();
+            return true;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+            return false;
+        }
+    }
 }
