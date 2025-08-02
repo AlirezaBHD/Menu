7a154fdc00e03ac8f7fd6c8ad935f08855a4cc6a
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-02 16:27:47 +0330

    Add Role entity with name, normalized name, and user roles collection.

---
+++ Domain/Entities/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities;
+
+public class Role : BaseEntity
+{
+    public string Name { get; set; }
+
+    public string NormalizedName { get; set; }
+
+    public ICollection<UserRole> Users { get; set; } = new List<UserRole>();
+}
