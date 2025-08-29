1493548b7487b834c216061bfbf229f86da23213
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-29 22:44:10 +0330

    feat: add multi-language support to menu item mapping
    This commit introduces multi-language support for menu item
    mapping by using the ForAllMultiLanguageMembers extension
    method. It also refactors the MenuItemListResponse mapping for
    better readability.

--- Application/Mappings/MenuItemProfile.cs
+++ Application/Mappings/MenuItemProfile.cs
@@ -9,11 +9,14 @@ public class MenuItemProfile : Profile
 {
     public MenuItemProfile()
     {
-        CreateMap<MenuItem, MenuItemDto>();
+        CreateMap<MenuItem, MenuItemDto>()
+            .ForAllMultiLanguageMembers();
+        
         CreateMap<MenuItem, MenuItemResponse>();
         CreateMap<CreateMenuItemRequest, MenuItem>();
         CreateMap<UpdateMenuItemRequest, MenuItem>();
-        CreateMap<MenuItem, MenuItemListResponse>().ForMember(dest => dest.SectionTitle, opt =>
+        CreateMap<MenuItem, MenuItemListResponse>()
+            .ForMember(dest => dest.SectionTitle, opt =>
             opt.MapFrom(src =>
                 src.Section!.Title)); 
     }
