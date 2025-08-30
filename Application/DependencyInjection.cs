2b2fddf0cbe219994da68c2075b1bc1cc92d9ba0
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-30 20:01:20 +0330

    feat: add current language service
    This commit adds the CurrentLanguage service and interface
    to the dependency injection container. This allows for
    easy access to the current language settings throughout
    the application.

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -19,6 +19,8 @@ public static class DependencyInjection
         services.AddScoped<ICurrentUser, CurrentUser>();
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<IUserService, UserService>();
+        services.AddScoped<ICurrentLanguage, CurrentLanguage>();
+
 
         return services;
     }
