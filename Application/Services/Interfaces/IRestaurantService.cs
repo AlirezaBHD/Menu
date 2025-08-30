2d91161df4eba5584b8aac80b3e4cb81c4331ddc
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-30 18:06:16 +0330

    fix: change restaurant id type from guid to int
    This commit updates the restaurant id type from Guid to int in
    the IRestaurantService interface. This change aligns with the
    database schema and improves performance.

--- Application/Services/Interfaces/IRestaurantService.cs
+++ Application/Services/Interfaces/IRestaurantService.cs
@@ -6,7 +6,7 @@ namespace Application.Services.Interfaces;
 public interface IRestaurantService : IService<Restaurant>
 {
     Task CreateRestaurantAsync(CreateRestaurantRequest createRestaurantRequest);
-    Task UpdateRestaurantAsync(Guid id, UpdateRestaurantRequest dto);
-    Task<RestaurantResponse> GetRestaurantByIdAsync(Guid id);
-    Task<IEnumerable<RestaurantMenuDto>> GetRestaurantMenuAsync(Guid restaurantId);
+    Task UpdateRestaurantAsync(int id, UpdateRestaurantRequest dto);
+    Task<RestaurantResponse> GetRestaurantByIdAsync(int id);
+    Task<IEnumerable<RestaurantMenuDto>> GetRestaurantMenuAsync(int restaurantId);
 }
