f7d5d8cf202ef8077826259918caf00963fb20b0
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-30 18:08:01 +0330

    fix: change MenuItemIds type from Guid to int
    The MenuItemIds property in the UpdateSectionRequest DTO was
    incorrectly defined as a list of Guids. This commit corrects
    the type to a list of integers, which aligns with the actual
    data being stored and used.

--- Application/Dto/Section/UpdateSectionRequest.cs
+++ Application/Dto/Section/UpdateSectionRequest.cs
@@ -5,6 +5,6 @@ namespace Application.Dto.Section;
 public class UpdateSectionRequest
 {
     public string Title { get; set; }
-    public List<Guid> MenuItemIds { get; set; }
+    public List<int> MenuItemIds { get; set; }
     public ActivityPeriodRequest ActivityPeriod { get; set; }
 }
