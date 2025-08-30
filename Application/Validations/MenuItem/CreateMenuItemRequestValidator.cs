8c8551ec59b0e9a8fec5120057ab52521ac531bd
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-30 22:05:46 +0330

    fix: correct namespace for menuitem entity
    The namespace for the MenuItem entity was incorrect, leading to
    compilation errors. This commit updates the namespace to the
    correct value: Domain.Entities.MenuItems.MenuItem.
    ```

--- Application/Validations/MenuItem/CreateMenuItemRequestValidator.cs
+++ Application/Validations/MenuItem/CreateMenuItemRequestValidator.cs
@@ -10,7 +10,7 @@ public class CreateMenuItemRequestValidator : AbstractValidator<CreateMenuItemRe
 {
     public CreateMenuItemRequestValidator()
     {
-        var entityType = typeof(Domain.Entities.MenuItem.MenuItem);
+        var entityType = typeof(Domain.Entities.MenuItems.MenuItem);
 
         RuleFor(c => c.Title)!
             .LengthValidationRule(dto => dto.Title!, entityType);
