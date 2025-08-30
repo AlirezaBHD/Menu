94cb6f535f4d497ea0367497805f6c379dfa1e79
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-30 20:02:48 +0330

    fix: change section id type from guid to int
    This commit changes the type of the Id property in the
    SectionListResponse class from Guid to int. This is to
    align with the database schema and simplify the codebase.

--- Application/Dto/Section/SectionListResponse.cs
+++ Application/Dto/Section/SectionListResponse.cs
@@ -4,7 +4,7 @@ namespace Application.Dto.Section;
 
 public class SectionListResponse
 {
-    public Guid Id { get; set; }
+    public int Id { get; set; }
     [Required]
     public string Title { get; set; }
     public int Order { get; set; }
