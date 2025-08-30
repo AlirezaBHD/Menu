3b17030fd7dbac9e565bdc764f913c2eab6570c9
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-30 18:06:20 +0330

    fix: change section id type from guid to int
    The section id type was changed from Guid to int in the
    ISectionService interface. This change was made to align
    with the database schema and improve performance.

--- Application/Services/Interfaces/ISectionService.cs
+++ Application/Services/Interfaces/ISectionService.cs
@@ -6,10 +6,10 @@ namespace Application.Services.Interfaces;
 
 public interface ISectionService : IService<Section>
 {
-    Task<SectionResponse> CreateSectionAsync(Guid categoryId, CreateSectionRequest createSectionRequest);
-    Task<SectionResponse> GetSectionByIdAsync(Guid sectionId);
-    Task DeleteSectionAsync(Guid id);
-    Task UpdateSectionAsync(Guid id, UpdateSectionRequest dto);
+    Task<SectionResponse> CreateSectionAsync(int categoryId, CreateSectionRequest createSectionRequest);
+    Task<SectionResponse> GetSectionByIdAsync(int sectionId);
+    Task DeleteSectionAsync(int id);
+    Task UpdateSectionAsync(int id, UpdateSectionRequest dto);
     Task<IEnumerable<SectionListResponse>> GetSectionListAsync();
     Task UpdateSectionOrderAsync(List<OrderDto> dto);
 }
