8361657702ab098bc05a27bbe56e8e5fe5c99c78
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-30 20:03:02 +0330

    fix: change id type from guid to int in MenuItemResponse

--- Application/Dto/MenuItem/MenuItemResponse.cs
+++ Application/Dto/MenuItem/MenuItemResponse.cs
@@ -4,7 +4,7 @@ namespace Application.Dto.MenuItem;
 
 public class MenuItemResponse
 {
-    public Guid Id { get; set; }
+    public int Id { get; set; }
     [Required]
     public string Title { get; set; }
 }
