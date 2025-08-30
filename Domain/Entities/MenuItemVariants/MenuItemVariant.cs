cc88f00b9a5d446a9a8c9ac40836434a440ac045
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-30 22:09:45 +0330

    refactor: move menu item variant entity to correct folder
    This commit moves the MenuItemVariant entity from the
    'Domain/Entities' folder to the
    'Domain/Entities/MenuItemVariants' folder to improve project
    structure and maintainability. Also MenuItemVariant entity was
    updated by removing unnessecary properties and adding
    Translations property.
    ```

--- Domain/Entities/MenuItemVariants/MenuItemVariant.cs
+++ Domain/Entities/MenuItemVariants/MenuItemVariant.cs
@@ -1,21 +1,17 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using Domain.Entities.MenuItems;
 
-namespace Domain.Entities;
+namespace Domain.Entities.MenuItemVariants;
 
 [DisplayName("نوع آیتم منو")]
 public class MenuItemVariant : BaseEntity
 {
     public int MenuItemId { get; set; }
-    public MenuItem.MenuItem MenuItem { get; set; } = null!;
-
-    [MaxLength(150)]
-    [Display(Name = "جزئیات")]
-    public string Detail { get; set; }  = "";
-
-    [Display(Name = "مبلغ")]
-    public decimal Price { get; set; }
+    public MenuItem MenuItem { get; set; } = null!;
 
     [Display(Name = "قابل ارائه بودن")]
     public bool IsAvailable { get; set; } = true;
+    
+    public ICollection<MenuItemVariantTranslation> Translations { get; set; } = new List<MenuItemVariantTranslation>();
 }
