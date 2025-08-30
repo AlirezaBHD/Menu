8685bb7f8a25897e6e7f9c7f88e5c054b4f6a290
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-30 22:04:42 +0330

    feat(category): move category entity to categories folder
    This commit moves the Category entity from the Domain/Entities
    folder to the Domain/Entities/Categories folder.
    It also updates the namespace and adds a navigation property
    to CategoryTranslation.
    ```

--- Domain/Entities/Categories/Category.cs
+++ Domain/Entities/Categories/Category.cs
@@ -1,18 +1,18 @@
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+using Domain.Entities.Restaurants;
+using Domain.Entities.Sections;
 
-namespace Domain.Entities;
+namespace Domain.Entities.Categories;
 
 [DisplayName("دسته بندی")]
 public class Category: BaseEntity
 {
-    [MaxLength(50)]
-    [Display(Name = "عنوان")]
-    public required string Title { get; set; }
     public ActivityPeriod ActivityPeriod { get; set; } = new();
     public int Order { get; set; }
     public int RestaurantId { get; set; }
 
     public Restaurant? Restaurant { get; set; }
     public ICollection<Section> Sections { get; set; } = new List<Section>();
+    
+    public ICollection<CategoryTranslation> Translations { get; set; } = new List<CategoryTranslation>();
 }
