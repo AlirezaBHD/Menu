c5717f8c04bcf9d8e890fd3d1599b58fe296fad7
AlirezaBHD <alirezabhd2@gmail.com>
2025-08-30 22:10:57 +0330

    feat: rename Section entity and update namespaces
    This commit renames the Section entity and updates the
    corresponding namespaces to improve project structure and
    maintainability.
    
    The following changes were made:
    
    - Renamed Section.cs to Sections/Section.cs.
    - Updated namespaces to reflect the new file location.
    - Updated MenuItem collection type.
    ```

---
+++ Domain/Entities/Sections/Section.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel;
+using Domain.Entities.Categories;
+using Domain.Entities.MenuItems;
+
+namespace Domain.Entities.Sections;
+
+[DisplayName("بخش")]
+public class Section: BaseEntity
+{
+    public ActivityPeriod ActivityPeriod { get; set; } = new();
+    public int Order { get; set; }
+    public int? CategoryId { get; set; }
+
+    public Category? Category { get; set; }
+    public ICollection<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+    
+    public ICollection<SectionTranslation> Translations { get; set; } = new List<SectionTranslation>();
+}
