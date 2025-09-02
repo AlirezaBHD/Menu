fdd18172647e96581147a480447a42d9e679e3ad
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-03 00:26:09 +0330

    feat: add multi-language property attribute to section title
    The SSLHandshakeException suggests potential issues with
    secure communication. While this commit doesn't directly
    address that, adding the `MultiLanguageProperty` attribute
    to the `Title` property of the `SectionResponse` DTO
    indicates a move towards internationalization, which might
    involve handling character encodings and potentially
    affecting secure communication in the future. This commit
    prepares the application for multi-language support by
    annotating the `Title` property.
    ```

--- Application/Dto/Section/SectionResponse.cs
+++ Application/Dto/Section/SectionResponse.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Application.Dto.ActivityPeriod;
 using Application.Dto.MenuItem;
+using Domain.Common.Attributes;
 
 namespace Application.Dto.Section;
 
@@ -8,6 +9,7 @@ public class SectionResponse
 {
     public int Id { get; set; }
     [Required]
+    [MultiLanguageProperty]
     public string Title { get; set; }
     public ActivityPeriodResponse ActivityPeriod { get; set; }
     public List<MenuItemResponse> MenuItems { get; set; }
