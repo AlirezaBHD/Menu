fe72cdf5c7d16cf90baa5c2fa168a2b9b9ff1229
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-03 00:23:32 +0330

    feat: eager load translations and sections for categories
    This commit modifies the CategoryService to eager load
    translations and sections for categories in the
    GetCategoryByIdAsync, UpdateCategoryAsync and
    GetCategoryListAsync methods. This improves performance by
    reducing the number of database queries required to retrieve
    category data.

--- Application/Services/CategoryService.cs
+++ Application/Services/CategoryService.cs
@@ -68,8 +68,16 @@ public class CategoryService : Service<Category>, ICategoryService
     
     public async Task<CategoryResponse> GetCategoryByIdAsync(int categoryId)
     {
+        
+        var query = Queryable.Include(c => c.Translations)
+            .Include(c => c.Sections).ThenInclude(s => s.Translations);
+        
         var response =
-            await GetByIdProjectedAsync<CategoryResponse>(categoryId, trackingBehavior: TrackingBehavior.AsNoTracking);
+            await GetByIdProjectedAsync<CategoryResponse>(categoryId,
+                query: query,
+                trackingBehavior: TrackingBehavior.AsNoTracking);
+
+
         return response;
     }
 
@@ -83,7 +91,8 @@ public class CategoryService : Service<Category>, ICategoryService
 
     public async Task UpdateCategoryAsync(int id, UpdateCategoryRequest dto)
     {
-        var category = await Repository.GetQueryable().Include(c => c.Sections)
+        var category = await Queryable.Include(c => c.Sections)
+            .Include(c => c.Translations)
             .FirstAsync(c => c.Id == id);
         
         category = Mapper.Map(dto, category);
@@ -95,8 +104,9 @@ public class CategoryService : Service<Category>, ICategoryService
             .ToListAsync();
         
         category.Sections = sections;
-
+        
         Repository.Update(category);
+
         await Repository.SaveAsync();
         Logger.LogInformation("Updated category with ID: {Id}. Data: {@UpdateData}", id, category);
     }
@@ -104,7 +114,9 @@ public class CategoryService : Service<Category>, ICategoryService
     public async Task<IEnumerable<CategoryListResponse>> GetCategoryListAsync()
     {
         var result =await GetAllProjectedAsync<CategoryListResponse>
-            (predicate:c => c.RestaurantId == _user.RestaurantId,trackingBehavior: TrackingBehavior.AsNoTracking);
+            (predicate:c => c.RestaurantId == _user.RestaurantId,
+                includes: [c => c.Translations],
+                trackingBehavior: TrackingBehavior.AsNoTracking);
         return result.OrderBy(c => c.Order);
     }
 
