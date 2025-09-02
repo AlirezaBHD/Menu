57e5d0f608ba3c79486b4695c9c325ac1aa4f34e
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-03 00:12:25 +0330

    feat: use base translation entity for menu item translation
    This commit refactors the MenuItemTranslation entity to inherit
    from BaseTranslationEntity<MenuItem>. This simplifies the class
    by removing redundant properties like MenuItemId, MenuItem,
    LanguageCode, and Language, as these are now handled by the base
    class.
    ```

--- Domain/Entities/MenuItems/MenuItemTranslation.cs
+++ Domain/Entities/MenuItems/MenuItemTranslation.cs
@@ -2,18 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities.MenuItems;
 
-public sealed class MenuItemTranslation : BaseEntity
+public sealed class MenuItemTranslation : BaseTranslationEntity<MenuItem>
 {
     [MaxLength(50)]
     public required string Title { get; set; }
     
     [MaxLength(300)]
     public string? Description { get; set; }
-
-    public int MenuItemId { get; set; }
-    public MenuItem MenuItem { get; set; } = default!;
-    
-    [MaxLength(10)]
-    public string LanguageCode { get; set; } = default!;
-    public Language Language { get; set; } = default!;
 }
