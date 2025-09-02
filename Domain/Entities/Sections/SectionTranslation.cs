34e15d14989aa32920616ef49749c255db363c79
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-03 00:12:14 +0330

    feat: use base translation entity for section translation
    This commit refactors the SectionTranslation entity to inherit
    from BaseTranslationEntity, removing redundant properties and
    simplifying the class structure.
    ```

--- Domain/Entities/Sections/SectionTranslation.cs
+++ Domain/Entities/Sections/SectionTranslation.cs
@@ -2,16 +2,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities.Sections;
 
-public class SectionTranslation : BaseEntity
+public class SectionTranslation : BaseTranslationEntity<Section>
 {
     [MaxLength(50)]
     [Display(Name = "عنوان")]
     public required string Title { get; set; }
-    
-    public int SectionId { get; set; }
-    public Section Section { get; set; } = default!;
-    
-    [MaxLength(10)]
-    public string LanguageCode { get; set; } = default!;
-    public Language Language { get; set; } = default!;
 }
