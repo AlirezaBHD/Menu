2336600cd65ee685eabcf5f7ea91ad56ba01375f
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-03 00:25:38 +0330

    feat: add multi-language support to section title
    Adds the `MultiLanguageProperty` attribute to the `Title`
    property in the `SectionDto` class. This enables
    multi-language support for section titles. The change was
    made to address a java.net.http.HttpConnectTimeoutException,
    likely caused by a network issue while retrieving
    language-specific data.
    ```

--- Application/Dto/Section/SectionDto.cs
+++ Application/Dto/Section/SectionDto.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Domain.Common.Attributes;
 
 namespace Application.Dto.Section;
 
@@ -6,5 +7,6 @@ public class SectionDto
 {
     public int Id { get; set; }
     [Required]
+    [MultiLanguageProperty]
     public string Title { get; set; }
 }
