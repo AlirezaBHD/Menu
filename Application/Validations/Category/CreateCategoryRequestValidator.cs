71c12bafda262ae52cf5e49e204db643644f6144
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-03 00:20:26 +0330

    feat: add category translation validation
    This commit introduces validation for category translations
    using the CategoryTranslationValidator. It ensures that each
    translation within the Translations collection is properly
    validated.
    ```

--- Application/Validations/Category/CreateCategoryRequestValidator.cs
+++ Application/Validations/Category/CreateCategoryRequestValidator.cs
@@ -9,13 +9,11 @@ public class CreateCategoryRequestValidator : AbstractValidator<CreateCategoryRe
 {
     public CreateCategoryRequestValidator()
     {
-        var entityType = typeof(Domain.Entities.Categories.Category);
-        
-        RuleFor(c => c.Title)!
-            .LengthValidationRule(dto => dto.Title!, entityType);
-        
         RuleFor(x => x.ActivityPeriod)
             .NotNull().WithMessage("دوره دسترسی الزامی است")
             .SetValidator(new ActivityPeriodDtoValidator());
+        
+        RuleForEach(x => x.Translations)
+            .SetValidator(new CategoryTranslationValidator());
     }
 }
