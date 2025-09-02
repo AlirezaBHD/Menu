7425ea22f42a5c56433febc534be691865b07342
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-03 00:14:13 +0330

    feat: add multi language property attribute to restaurant dto
    This commit adds the `MultiLanguageProperty` attribute to the
    `Name`, `Address`, and `Description` properties of the
    `RestaurantResponse` DTO. Also added `Id` property.
    This enhancement enables the application to support
    multiple languages for restaurant information.

--- Application/Dto/Restaurant/RestaurantResponse.cs
+++ Application/Dto/Restaurant/RestaurantResponse.cs
@@ -1,13 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Domain.Common.Attributes;
 
 namespace Application.Dto.Restaurant;
 
 public class RestaurantResponse
 {
+    public int Id { get; set; }
     [Required]
+    [MultiLanguageProperty]
     public string Name { get; set; }
     [Required]
+    [MultiLanguageProperty]
     public string Address { get; set; }
+    [MultiLanguageProperty]
     public string? Description { get; set; }
     public string? LogoPath { get; set; }
     [Required]
