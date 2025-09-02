7f498328cf14b6b86de8d96daaa296ec80066c04
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-03 00:30:49 +0330

    feat: add menu item translation validator
    Adds a validator for menu item translations and applies
    it to the update menu item request validator.
    ```

--- Application/Validations/MenuItem/UpdateMenuItemRequestValidator.cs
+++ Application/Validations/MenuItem/UpdateMenuItemRequestValidator.cs
@@ -9,18 +9,13 @@ public class UpdateMenuItemRequestValidator : AbstractValidator<UpdateMenuItemRe
 {
     public UpdateMenuItemRequestValidator()
     {
-        var entityType = typeof(Domain.Entities.MenuItems.MenuItem);
-
-        RuleFor(c => c.Title)!
-            .LengthValidationRule(dto => dto.Title, entityType);
-
-        RuleFor(c => c.Description)!
-            .LengthValidationRule(dto => dto.Description!, entityType, blank: true);
-
-        RuleFor(c => c.ImageFile)!.ImageFileRule(blank: true);
+        // RuleFor(c => c.ImageFile)!.ImageFileRule(blank: true);
         
         RuleFor(x => x.ActivityPeriod)
             .NotNull().WithMessage("دوره دسترسی الزامی است")
             .SetValidator(new ActivityPeriodDtoValidator());
+        
+        RuleForEach(x => x.Translations)
+            .SetValidator(new MenuItemTranslationValidator());
     }
 }
