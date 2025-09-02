beaec649a909fb59cf12a133b0e6c458891648e6
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-03 00:19:24 +0330

    feat(category): add multi-language support to title
    This commit adds the `MultiLanguageProperty` attribute to the
    `Title` property in the `CategoryResponse` DTO. Also made
    `Title` and `ActivityPeriod` nullable.
    ```

--- Application/Dto/Category/CategoryResponse.cs
+++ Application/Dto/Category/CategoryResponse.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Application.Dto.ActivityPeriod;
 using Application.Dto.Section;
+using Domain.Common.Attributes;
 
 namespace Application.Dto.Category;
 
@@ -8,7 +9,8 @@ public class CategoryResponse
 {
     public int Id { get; set; }
     [Required]
-    public string Title { get; set; }
-    public ActivityPeriodResponse ActivityPeriod { get; set; }
+    [MultiLanguageProperty]
+    public string? Title { get; set; }
+    public ActivityPeriodResponse? ActivityPeriod { get; set; }
     public List<SectionDto> Sections { get; set; } = [];
 }
