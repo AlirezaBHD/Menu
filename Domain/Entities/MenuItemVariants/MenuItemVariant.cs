21f3694fc086485460206c767d8a0c1acffa2bc4
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-03 00:11:06 +0330

    feat: implement translation interface for menu item variant
    This commit implements the `ITranslation` interface for the
    `MenuItemVariant` entity. This allows for the translation of
    menu item variant properties into different languages.

--- Domain/Entities/MenuItemVariants/MenuItemVariant.cs
+++ Domain/Entities/MenuItemVariants/MenuItemVariant.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using Domain.Entities.MenuItems;
+using Domain.Interfaces;
 
 namespace Domain.Entities.MenuItemVariants;
 
 [DisplayName("نوع آیتم منو")]
-public class MenuItemVariant : BaseEntity
+public class MenuItemVariant : BaseEntity, ITranslation<MenuItemVariantTranslation>
 {
     public int MenuItemId { get; set; }
     public MenuItem MenuItem { get; set; } = null!;
