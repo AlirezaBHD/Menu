066d35d9348a96d2a6776a0e0c1da89bf25c2fae
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-03 00:11:49 +0330

    feat: refactor category translation entity
    This commit refactors the CategoryTranslation entity to inherit
    from BaseTranslationEntity<Category>. This removes redundant
    properties like CategoryId, Category, LanguageCode, and
    Language, as these are now handled by the base class.
    ```

--- Domain/Entities/Categories/CategoryTranslation.cs
+++ Domain/Entities/Categories/CategoryTranslation.cs
@@ -2,16 +2,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities.Categories;
 
-public class CategoryTranslation : BaseEntity
+public class CategoryTranslation : BaseTranslationEntity<Category>
 {
     [MaxLength(50)]
     [Display(Name = "عنوان")]
     public required string Title { get; set; }
-    
-    public int CategoryId { get; set; }
-    public Category Category { get; set; } = default!;
-    
-    [MaxLength(10)]
-    public string LanguageCode { get; set; } = default!;
-    public Language Language { get; set; } = default!;
 }
