7dedc8351932962ef8866e608366558ad85e7ff4
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-05 17:05:06 +0330

    feat: add image file property to create menu item request
    This commit adds the `ImageFile` property to the
    `CreateMenuItemRequest` class, allowing users to upload
    an image when creating a new menu item.
    
    Also added ModelBinder attribute to Translations property.

--- Application/Dto/MenuItem/CreateMenuItemRequest.cs
+++ Application/Dto/MenuItem/CreateMenuItemRequest.cs
@@ -2,18 +2,20 @@ using Application.Dto.ActivityPeriod;
 using Application.Dto.MenuItemVariant;
 using Application.Dto.Shared;
 using Application.Extensions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Dto.MenuItem;
 
 public class CreateMenuItemRequest : IHasTranslationsDto<MenuItemTranslationDto>
 {
-    // public IFormFile ImageFile { get; set; }
+    public IFormFile ImageFile { get; set; }
     public bool IsAvailable { get; set; }
     public ActivityPeriodRequest ActivityPeriod { get; set; }
     
     [ModelBinder(BinderType = typeof(JsonModelBinder))]
     public List<MenuItemVariantDto> Variants { get; set; }
 
+    [ModelBinder(BinderType = typeof(JsonModelBinder))]
     public ICollection<MenuItemTranslationDto> Translations { get; set; } = new List<MenuItemTranslationDto>();
 }
