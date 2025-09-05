0ff711ee57c8ab2bfa40be2e1f0a3649c9e5bb1a
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-05 17:02:32 +0330

    feat: enable logo upload and json model binding
    This commit enables the upload of a restaurant logo file and
    configures JSON model binding for the translations collection.
    
    - Added IFormFile property for logo upload.
    - Configured JSON model binding for the Translations property.

--- Application/Dto/Restaurant/CreateRestaurantRequest.cs
+++ Application/Dto/Restaurant/CreateRestaurantRequest.cs
@@ -1,5 +1,6 @@
 using Application.Dto.Shared;
 using Application.Extensions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Dto.Restaurant;
@@ -7,9 +8,10 @@ namespace Application.Dto.Restaurant;
 public class CreateRestaurantRequest : IHasTranslationsDto<RestaurantTranslationDto>
 {
     public int OwnerId { get; set; }
-    // public IFormFile LogoFile { get; set; }
+    public IFormFile LogoFile { get; set; }
     
     public Dictionary<string, string> OpeningHours { get; set; }
     
-    public ICollection<RestaurantTranslationDto> Translations { get; set; } = new List<RestaurantTranslationDto>();
+    [ModelBinder(BinderType = typeof(JsonModelBinder))]
+    public ICollection<RestaurantTranslationDto> Translations { get; set; }
 }
