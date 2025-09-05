cb8f79c1a1c1ffae231f66ea4990248d74fa9ba3
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-05 13:42:24 +0330

    feat: use localize display attribute for activity period
    This commit replaces the Display attribute with the
    LocalizeDisplay attribute in the ActivityPeriod entity
    and ActivityEnum enum. This allows the display names to be
    localized.

--- Domain/Entities/ActivityPeriod.cs
+++ Domain/Entities/ActivityPeriod.cs
@@ -1,30 +1,31 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Domain.Common.Attributes;
+using Domain.Localization;
 
 namespace Domain.Entities;
 
 public class ActivityPeriod
 {
-    [Display(Name = "فعال بودن")]
+    [LocalizeDisplay(nameof(Resources.IsActive))]
     public bool IsActive { get; set; } = true;
-    
-    [Display(Name = "وضعیت فعالیت")]
+
+    [LocalizeDisplay(nameof(Resources.ActivityType))]
     public ActivityEnum ActivityType { get; set; } = ActivityEnum.Unlimited;
-    
-    [Display(Name = "ساعت شروع")]
+
+    [LocalizeDisplay(nameof(Resources.FromTime))]
     public TimeSpan FromTime { get; set; } = TimeSpan.Zero;
-    
-    [Display(Name = "ساعت پایان")]
+
+    [LocalizeDisplay(nameof(Resources.ToTime))]
     public TimeSpan ToTime { get; set; } = TimeSpan.FromHours(24);
 }
 
 public enum ActivityEnum
 {
-    [Display(Name = "بدون محدودیت")]
+    [LocalizeDisplay(nameof(Resources.Unlimited))]
     Unlimited = 0,
-    
-    [Display(Name = "بازه فعال بودن")]
+
+    [LocalizeDisplay(nameof(Resources.ActivePeriod))]
     ActivePeriod = 1,
-    
-    [Display(Name = "بازه غیرفعال بودن")]
+
+    [LocalizeDisplay(nameof(Resources.InactivePeriod))]
     InactivePeriod = 2
 }
