e9710ea2b37dacf828b3ee891379f5ee3612183a
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-05 17:04:20 +0330

    feat: add logo file and json model binder to update request
    This commit adds the LogoFile property to the
    UpdateRestaurantRequest class to allow updating the restaurant
    logo. It also includes the JsonModelBinder attribute to handle
    complex object binding for the Translations property.

--- Application/Dto/Restaurant/UpdateRestaurantRequest.cs
+++ Application/Dto/Restaurant/UpdateRestaurantRequest.cs
@@ -1,13 +1,16 @@
 using Application.Dto.Shared;
+using Application.Extensions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Dto.Restaurant;
 
 public class UpdateRestaurantRequest : IHasTranslationsDto<RestaurantTranslationDto>
 {
-    // public IFormFile? LogoFile { get; set; }
+    public IFormFile? LogoFile { get; set; }
     public Dictionary<string, string> OpeningHours { get; set; }
 
+    [ModelBinder(BinderType = typeof(JsonModelBinder))]
     public ICollection<RestaurantTranslationDto> Translations { get; set; } =
         new List<RestaurantTranslationDto>();
 }
