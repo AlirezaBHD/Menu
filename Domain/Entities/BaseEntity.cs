daaddbfd863c78312cfac51619a6b664f6435d4f
AlirezaBHD <alirezabhd2@gmail.com>
2025-09-05 13:41:53 +0330

    feat: use localize display attribute for base entity props
    This commit replaces the `Display` attribute with the
    `LocalizeDisplay` attribute for the `Id`, `CreatedOn`, and
    `ModifiedOn` properties in the `BaseEntity` class. This allows
    for localization of the display names in the UI.

--- Domain/Entities/BaseEntity.cs
+++ Domain/Entities/BaseEntity.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Common.Attributes;
+using Domain.Localization;
 
 namespace Domain.Entities;
 
@@ -7,12 +9,12 @@ public abstract class BaseEntity
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    [Display(Name = "شناسه")]
+    [LocalizeDisplay(nameof(Resources.Identity))]
     public int Id { get; set; }
     
-    [Display(Name = "زمان ایجاد")]
+    [LocalizeDisplay(nameof(Resources.CreateDate))]
     public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
     
-    [Display(Name = "زمان آخرین تغییر")]
+    [LocalizeDisplay(nameof(Resources.ModifyDate))]
     public DateTime ModifiedOn { get; set; } = DateTime.UtcNow;
 }
