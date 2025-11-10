645fa64115851beffc1ff4cddf111638ed27419c
AlirezaBHD <alirezabhd2@gmail.com>
2025-11-10 05:49:00 +0330

    refactor: update create restaurant request structure
    - Replace owner id and logo file with activity period
    - Use ActivityPeriodRequest for availability management
    - Remove IFormFile dependency from main request

--- Application/Dto/Restaurant/CreateRestaurantRequest.cs
+++ Application/Dto/Restaurant/CreateRestaurantRequest.cs
@@ -1,14 +1,13 @@
+using Application.Dto.ActivityPeriod;
 using Application.Dto.Shared;
 using Application.Extensions;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Dto.Restaurant;
 
 public class CreateRestaurantRequest : IHasTranslationsDto<RestaurantTranslationDto>
 {
-    public int OwnerId { get; set; }
-    public IFormFile LogoFile { get; set; }
+    public ActivityPeriodRequest ActivityPeriod { get; set; }
     
     public Dictionary<string, string> OpeningHours { get; set; }
     
