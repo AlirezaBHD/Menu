442d6a6ba8fb985c6ec6a8f2653b23a01abd4cd6
AlirezaBHD <alirezabhd2@gmail.com>
2025-11-10 06:06:53 +0330

    feat: extend restaurant service with new operations and return types
    - Change CreateRestaurantAsync to return ResponseDto
    - Update GetRestaurantMenuAsync to return single RestaurantMenuDto
    - Add image editing, order update, and detail methods
    - Include new DTO dependencies and return types

--- Application/Services/Interfaces/IRestaurantService.cs
+++ Application/Services/Interfaces/IRestaurantService.cs
@@ -1,12 +1,17 @@
 using Application.Dto.Restaurant;
+using Application.Dto.Shared;
 using Domain.Entities.Restaurants;
 
 namespace Application.Services.Interfaces;
 
 public interface IRestaurantService : IService<Restaurant>
 {
-    Task CreateRestaurantAsync(CreateRestaurantRequest createRestaurantRequest);
+    Task<ResponseDto> CreateRestaurantAsync(CreateRestaurantRequest createRestaurantRequest);
     Task UpdateRestaurantAsync(int id, UpdateRestaurantRequest dto);
     Task<RestaurantResponse> GetRestaurantByIdAsync(int id);
-    Task<IEnumerable<RestaurantMenuDto>> GetRestaurantMenuAsync(int restaurantId);
+    Task<RestaurantMenuDto> GetRestaurantMenuAsync(int restaurantId);
+    Task<string> EditImageAsync(int id, ImageDto image);
+    Task UpdateRestaurantOrderAsync(List<OrderDto> dto);
+    Task<IEnumerable<RestaurantDto>> RestaurantDetailList();
+    Task<RestaurantDetailDto> RestaurantDetail(int id);
 }
