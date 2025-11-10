c6a659ad320debe50bae0e621271dd42fd3ef6da
AlirezaBHD <alirezabhd2@gmail.com>
2025-11-10 06:16:04 +0330

    style: remove blank line in restaurant translation dto
    - Clean up code formatting by removing empty line between properties

--- Application/Dto/Restaurant/RestaurantTranslationDto.cs
+++ Application/Dto/Restaurant/RestaurantTranslationDto.cs
@@ -5,7 +5,6 @@ namespace Application.Dto.Restaurant;
 public class RestaurantTranslationDto: TranslationDto
 {
     public string Name { get; set; }
-
     public string Address { get; set; }
     public string? Description { get; set; }
 }
