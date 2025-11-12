8331f3b585c5725d476ec51ea56a398bd6bfd6ec
AlirezaBHD <a.bahmanzade.dev@gmail.com>
2025-11-12 08:39:58 +0330

    feat: register all extracted configurations in Program.cs
    - Chain all service extensions: AddAppCors, AddAppControllers, etc.
    - Use app.UseAppMiddlewares() for middleware pipeline
    - Clean and organized Program.cs with all extracted configurations

--- API/Program.cs
+++ API/Program.cs
@@ -1,191 +1,35 @@
-using System.Globalization;
-using System.Threading.RateLimiting;
 using API.Configurations;
-using API.Middlewares;
-using API.Utilities;
 using Application;
-using Application.Exceptions;
-using Application.Services;
-using Application.Services.Interfaces;
-using Application.Validations.Category;
-using Domain.Interfaces.Services;
-using Domain.Localization;
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using Infrastructure.Persistence;
-using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
-using Microsoft.EntityFrameworkCore;
-using Serilog;
-using Serilog.Formatting.Compact;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+builder.Host.UseAppLogging(builder.Configuration);
 
-var frontServer = builder.Configuration["FrontEnd:Url"];
+services
+    .AddAppCors(configuration)
+    .AddAppControllers()
+    .AddAppDbContext(configuration)
+    .AddAppRateLimiting()
+    .AddAppSession()
+    .AddAppLocalization()
+    .AddSwaggerDocumentation(configuration)
+    .AddApplicationServices()
+    .AddApplicationRepositories();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy
-            .AllowCredentials()
-            .WithOrigins(frontServer!)
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-    });
-});
-
-builder.Services.AddControllers(options => { options.ModelValidatorProviders.Clear(); });
-
-builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddValidatorsFromAssemblyContaining<CreateCategoryRequestValidator>();
-
-builder.Services.AddOpenApi();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<ICurrentUser, CurrentUser>();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddFluentValidationRulesToSwagger();
-
-builder.Services.AddControllers(options =>
-{
-    options.Conventions.Add(new RouteTokenTransformerConvention(new KebabCaseParameterTransformer()));
-});
-
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(
-            builder.Configuration.GetConnectionString("DefaultConnection"),
-            npgsqlOptions => npgsqlOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
-        )
-        .UseSnakeCaseNamingConvention());
-
-builder.Services.AddAutoMapper(typeof(IMenuItemService).Assembly);
-
-builder.Services.AddApplicationServices();
-builder.Services.AddApplicationRepositories();
-
-
-#region Logging
-
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration)
-    .Enrich.FromLogContext()
-    .WriteTo.File(
-        new CompactJsonFormatter(),
-        Path.Combine("..", "Logs", "log-.json"),
-        rollingInterval: RollingInterval.Day,
-        fileSizeLimitBytes: 10 * 1024 * 1024,
-        rollOnFileSizeLimit: true,
-        retainedFileCountLimit: 30,
-        shared: true
-    )
-    .CreateLogger();
-
-builder.Host.UseSerilog();
-
-#endregion
-
-#region Rate Limit
-
-builder.Services.AddRateLimiter(options =>
-{
-    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
-    {
-        var ip = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
-        return RateLimitPartition.GetFixedWindowLimiter(ip, _ => new FixedWindowRateLimiterOptions
-        {
-            PermitLimit = 20,
-            Window = TimeSpan.FromSeconds(10),
-            AutoReplenishment = true,
-            QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
-            QueueLimit = 0
-        });
-    });
-
-    options.RejectionStatusCode = 429;
-});
-
-#endregion
-
-#region Session
-
-builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromDays(1);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-    options.Cookie.SameSite = SameSiteMode.None;
-    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-});
-
-#endregion
-
-builder.Services.AddSwaggerDocumentation(builder.Configuration);
-builder.Services.AddAuthorization();
-
-builder.Services.Configure<RequestLocalizationOptions>(options =>
-{
-    var supportedCultures = SupportedLanguages.All.Select(l => l.Code).ToArray();
-    
-    options.DefaultRequestCulture = new RequestCulture(supportedCultures.First());
-    options.SupportedCultures = supportedCultures.Select(c => new CultureInfo(c)).ToList();
-    options.SupportedUICultures = supportedCultures.Select(c => new CultureInfo(c)).ToList();
-
-    options.RequestCultureProviders.Insert(0, new CookieRequestCultureProvider());
-    options.RequestCultureProviders.Insert(1, new QueryStringRequestCultureProvider());
-});
-
-builder.Services.AddHttpContextAccessor();
+services.AddAuthorization();
+services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseStaticFiles();
     app.UseSwaggerDocumentation();
 }
 
-
-
-
-using (var scope = app.Services.CreateScope())
-{
-    var currentLang = scope.ServiceProvider.GetRequiredService<ICurrentLanguage>();
-    MultiLanguageMappingExtensions.Configure(currentLang);
-}
-
-app.UseSession();
-
-app.UseRequestLocalization();
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseCors("AllowAll");
-
+app.ConfigureApplicationLanguage();
 app.MapControllers();
-
-app.UseRateLimiter();
-
-app.UseMiddleware<UserIdEnricherMiddleware>();
-app.UseMiddleware<ExceptionHandlingMiddleware>();
-
-app.Use(async (context, next) =>
-{
-    context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
-    context.Response.Headers.Append("X-Frame-Options", "DENY");
-    context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
-    await next();
-});
+app.UseAppMiddlewares();
 
 app.Run();
